ad1a1aad9af57eedf26bade10283251223fbe89f
xbgmsharp <flacroix>
2020-09-10 18:13:27 -0400

    Add Travis CI build

--- build.sh
+++ build.sh
@@ -13,8 +13,8 @@ echo "QEMU downloading ..."
 # Download latest QEMU to add per image arch
 QEMU_ARCH="x86_64 arm aarch64 i386"
 for target_arch in ${QEMU_ARCH}; do
-#  wget -Nq https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_RELEASE}/x86_64_qemu-${target_arch}-static.tar.gz
-#  tar -xvf x86_64_qemu-${target_arch}-static.tar.gz
+  wget -Nq https://github.com/multiarch/qemu-user-static/releases/download/${QEMU_RELEASE}/x86_64_qemu-${target_arch}-static.tar.gz
+  tar -xvf x86_64_qemu-${target_arch}-static.tar.gz
 done
 
 ls qemu-*
@@ -32,21 +32,21 @@ for arch in ${IMAGE_ARCH}; do
   then
 	QEMU_ARCH="aarch64"
   fi
-#  docker build \
-#         --build-arg ARCH=${arch} \
-#         --build-arg QEMU_BIN=qemu-${QEMU_ARCH}-static \
-#         -t $DOCKER_USER/docker-gramps:${arch} \
-#         -f gramps.Dockerfile .
   docker build \
          --build-arg ARCH=${arch} \
          --build-arg QEMU_BIN=qemu-${QEMU_ARCH}-static \
-         -t $DOCKER_USER/docker-gramps-webapp:${QEMU_ARCH} \
+         -t $DOCKER_USER/docker-gramps:${arch} \
+         -f gramps.Dockerfile .
+  docker build \
+         --build-arg ARCH=${arch} \
+         --build-arg QEMU_BIN=qemu-${QEMU_ARCH}-static \
+         -t $DOCKER_USER/docker-gramps-webapp:${arch} \
          -f gramps-webapp.Dockerfile .
-#  docker build \
-#         --build-arg ARCH=${arch} \
-#         --build-arg QEMU_BIN=qemu-${QEMU_ARCH}-static \
-#         -t $DOCKER_USER/docker-gramps-webapi:${arch} \
-#         -f gramps-webapi.Dockerfile .
+  docker build \
+         --build-arg ARCH=${arch} \
+         --build-arg QEMU_BIN=qemu-${QEMU_ARCH}-static \
+         -t $DOCKER_USER/docker-gramps-webapi:${arch} \
+         -f gramps-webapi.Dockerfile .
 done
 
 echo "Manifest downloading ..."
